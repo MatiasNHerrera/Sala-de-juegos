192468c1c5f2fd7438d6c2ec11312ee984d9ac96
matias <matiassh20@gmail.com>
2020-04-30 17:15:56 -0300

    juegos terminados

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -41,7 +41,6 @@ export class LoginComponent implements OnInit {
 
   validarCorreo() : boolean
   {
-
     let retorno = false;
     let regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
 
